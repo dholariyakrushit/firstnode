f74d280353d8491d9651b9cd7c40a0135c86dc91
Krushit <krushit@pop-os.localdomain>
2022-12-25 21:05:09 +0530

    simple

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path=require("path");
 const hbs=require("hbs");
 const requests=require("requests");
 
+
+
 const staticPath=path.join(__dirname,"../public");
 const templatesPath=path.join(__dirname,"../templates/views")
 const partialPath=path.join(__dirname,"../templates/partials")
@@ -17,23 +19,24 @@ app.set("views",templatesPath);
 app.set("view engine","hbs");
 hbs.registerPartials(partialPath);
 
+
 app.get("",(req,res)=>{
     res.render("index.hbs",{
         name:"krushit"
-    });
+    }); 
 });
 
 app.get("/about",(req,res)=>{
     requests(
-        `https://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=2f833bac4ee36479106711021b79cabf`
+        `https://api.openweathermap.org/data/2.5/weather?q=pune&units=metric&appid=2f833bac4ee36479106711021b79cabf`
       )
         .on("data", (chunk) => {
           const objdata = JSON.parse(chunk);
           const arrData = [objdata];
-          console.log(`city name ${arrData[0].name} and the temp is `);
-        //   ${arrData[0].main.temp}
-          res.write(arrData[0].main);
-          // console.log(realTimeData);
+          console.log(`city name ${arrData[0].name} and the temp is ${arrData[0].main.temp}`);
+          res.write(`city name ${arrData[0].name} and the temp is ${arrData[0].main.temp}`);
+          // res.write((arrData[0].main.temp).toString()); 
+          res.end();
         })
         .on("end", (err) => {
           if (err) return console.log("connection closed due to errors", err);
@@ -56,4 +59,4 @@ app.get("*",(req,res)=>{
     });
 });
 
-app.listen(5000);
+app.listen(5000); 
